7cc5911a8f8c7522d5e20e4cd629b7369c59d9ef
hamza azzab <azzab.hamzaa@gmail.com>
2024-06-04 21:07:47 +0100

    code

---
+++ src/main/java/ma/newserver/githubcicdactions/GithubCicdActionsApplication.java
@@ -0,0 +1,26 @@
+package ma.newserver.githubcicdactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GithubCicdActionsApplication {
+    public String welcome(){
+        return "Hamza lwa3err is here on the server li dwa ndih lblyssa";
+    }
+
+   /* echo "# github-actions-test" >> README.md
+    git init
+    git add README.md
+    git commit -m "first commit"
+    git branch -M main
+    git remote add origin https://github.com/amaAzz/github-actions-test.git
+    git push -u origin main*/
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubCicdActionsApplication.class, args);
+    }
+
+}
