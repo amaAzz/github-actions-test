9c7a08d4f8540e6646482c40db5830994a982d45
hamza azzab <azzab.hamzaa@gmail.com>
2024-06-05 12:58:53 +0100

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	Dockerfile

--- src/main/java/ma/newserver/githubcicdactions/GithubCicdActionsApplication.java
+++ src/main/java/ma/newserver/githubcicdactions/GithubCicdActionsApplication.java
@@ -2,10 +2,13 @@ package ma.newserver.githubcicdactions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("/")
 public class GithubCicdActionsApplication {
 
     public String welcome(){
